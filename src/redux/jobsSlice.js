b9316f43ddd3a98ef36ed20eade77a479d754315
MohitZNegi <mohit26negi@gmail.com>
2026-02-14 20:51:56 +1300

    implemented jobSlice using Redux Toolkit
     added Redux jobs slice with async job fetching and save/remove functionality

---
+++ src/redux/jobsSlice.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  jobs: [],
+  savedJobs: [],
+  status: 'idle', // idle | loading | succeeded | failed
+  error: null,
+  searchQuery: '',
+};
+
+export const fetchJobs = createAsyncThunk(
+  'jobs/fetchJobs',
+  async (query) => {
+    const response = await axios.get(
+      `https://remotive.com/api/remote-jobs?search=react=${query}`
+    );
+    return response.data.jobs;
+  }
+);
+
+const jobsSlice = createSlice({
+  name: 'jobs',
+  initialState,
+  reducers: {
+    setSearchQuery: (state, action) => {
+      state.searchQuery = action.payload;
+    },
+    saveJob: (state, action) => {
+      state.savedJobs.push(action.payload);
+    },
+    removeSavedJob: (state, action) => {
+      state.savedJobs = state.savedJobs.filter(
+        (job) => job.id !== action.payload.id
+      );
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchJobs.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchJobs.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.jobs = action.payload;
+      })
+      .addCase(fetchJobs.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { setSearchQuery, saveJob, removeSavedJob } = jobsSlice.actions;
+
+export default jobsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import jobsReducer from './jobsSlice';
 
 export const store = configureStore({
   reducer: {
+    jobs: jobsReducer,
   },
 });
 
